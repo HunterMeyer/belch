39c2cfa6f397d11cc523b665b88014e0d2ca1c3e
HunterMeyer <huntermeyer@live.com>
2017-09-23 20:32:30 -0400

    Create brewerydb_glasswares

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170924002634) do
+ActiveRecord::Schema.define(version: 20170924003135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,13 @@ ActiveRecord::Schema.define(version: 20170924002634) do
   end
 
   create_table "brewerydb_categories", force: :cascade do |t|
+    t.text     "external_id"
+    t.text     "name"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "brewerydb_glasswares", force: :cascade do |t|
     t.text     "external_id"
     t.text     "name"
     t.datetime "created_at",  null: false
