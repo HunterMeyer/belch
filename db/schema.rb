2dff496772408d036df06c7a6f4e8f696d44ae79
HunterMeyer <huntermeyer@live.com>
2017-09-23 20:02:41 -0400

    Create brewerydb_availabilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170923220954) do
+ActiveRecord::Schema.define(version: 20170923235114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "brewerydb_availabilities", force: :cascade do |t|
+    t.text     "external_id"
+    t.text     "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "brewerydb_beers", force: :cascade do |t|
     t.text     "external_id"
