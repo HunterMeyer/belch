9b37f875232bbc642ba08e80c45fc93e0544abbf
HunterMeyer <huntermeyer@live.com>
2017-09-29 00:02:14 -0400

    Trainer model and controller

---
+++ app/controllers/trainers_controller.rb
@@ -0,0 +1,45 @@
+class TrainersController < ApplicationController
+  def show
+    current_trainer
+  end
+
+  def new
+    @trainer = Trainer.new
+  end
+
+  def create
+    @trainer = Trainer.create(trainer_params)
+    if @trainer.save
+      redirect_to trainer_path(@trainer.id)
+    else
+      @error = 'Identifier already taken. Please try another'
+      render :new
+    end
+  end
+
+  def beers
+    offset = params[:page].to_i * 20
+    @beers = current_trainer.unrated_beers.limit(20).offset(offset)
+    render json: @beers
+  end
+
+  def rating
+    options = {
+      trainer:          current_trainer,
+      beer_external_id: params[:beer_id],
+      rating:           params[:rating]
+    }
+    @rating = TrainerRating.create(options)
+    render json: :ok
+  end
+
+  private
+
+  def trainer_params
+    params.require(:trainer).permit(:identifier)
+  end
+
+  def current_trainer
+    @trainer ||= Trainer.find(params[:id])
+  end
+end
--- app/models/trainer.rb
+++ app/models/trainer.rb
@@ -1,3 +1,8 @@
 class Trainer < ApplicationRecord
   validates :identifier, presence: true, uniqueness: { case_sensitive: false }
+  has_many  :ratings, class_name: TrainerRating.name
+
+  def unrated_beers
+    BrewerydbBeer.where('external_id NOT IN (?)', ratings.map(&:beer_external_id))
+  end
 end
