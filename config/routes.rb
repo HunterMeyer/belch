afa787c6393af96a4f443bd2d300989285c0efbd
HunterMeyer <huntermeyer@live.com>
2017-09-29 00:02:28 -0400

    Wip trainer views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-
+  resources :trainers, only: [:new, :create, :show] do
+    member do
+      get :beers,   defaults: { format: :json }
+      post :rating, defaults: { format: :json }
+    end
+  end
 end
